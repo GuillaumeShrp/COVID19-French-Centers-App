a8ef3ac8995f392172606983a3d2e9f4f46d1d8b
GuillaumeShrp <guillaume.sherpa@gmail.com>
2020-11-28 15:58:32 +0100

    Pretty Icon Launcher Update

--- android/app/src/main/java/com/ismin/projectapp/MainActivity.kt
+++ android/app/src/main/java/com/ismin/projectapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.ismin.projectapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    private fun displayErrorToast(t: Throwable) {
+        Toast.makeText(
+            applicationContext,
+            "Network error ${t.localizedMessage}",
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
 }
