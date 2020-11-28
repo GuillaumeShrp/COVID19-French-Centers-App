a513f53409ffda68a406de4de45c2d3d655bfbd9
Vivien Debuchy <viviendebuchy@yahoo.fr>
2020-11-28 18:19:59 +0100

    Import du fichier JSON URL pour un get all

---
+++ api/src/CovidTestCenter.ts
@@ -0,0 +1,17 @@
+
+export interface CovidTestCenter {
+  ID:            string;
+  rs:            string; //raison sociale
+  adresse:       string;
+  do_prel:       string;
+  do_antigenic:  string;
+  longitude:     string;
+  latitude:      string;
+  mod_prel:      string;
+  public:        string;
+  horaire:       string;
+  check_rdv:     string;
+  tel_rdv:       string;
+  web_rdv:       string;
+  date_modif:    string;
+}
